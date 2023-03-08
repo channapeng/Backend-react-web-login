6dbe8322cf19ab825729d8d12e94f35043c78624
channapeng <90314916+channapeng@users.noreply.github.com>
2023-03-08 12:37:54 +0700

    Add files via upload

--- configs/db_mongo.js
+++ configs/db_mongo.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.DATABASE)
+    await mongoose.connect(process.env.DATABASE_NAME)
     console.log("Connect DB Success")
   }catch(err) {
     console.log(err)
